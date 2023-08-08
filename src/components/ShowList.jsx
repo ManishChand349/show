5f80f0791c4c6fc83b750db4675da7bad6090838
Manish Chand <manishchandr349@gmail.com>
2023-08-08 18:13:58 +0530

    adding

--- src/components/ShowList.jsx
+++ src/components/ShowList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {Link } from 'react-router-dom';
-import './showList.css' 
+import './ShowList.css' 
 
 const URL = 'https://api.tvmaze.com/search/shows?q=all'
 
